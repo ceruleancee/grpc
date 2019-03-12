0b986cc94176fe580afed501988386946d44ce23
Claudia Ortiz <claudia@graylog.com>
2019-03-12 16:53:10 -0500

    Rename MyGrpcServer to GrpcServer

--- src/main/java/com/example/grpc/GrpcServer.java
+++ src/main/java/com/example/grpc/GrpcServer.java
@@ -1,19 +1,18 @@
 package com.example.grpc;
-
-import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.IOException;
 
-public class MyGrpcServer {
+public class GrpcServer {
 
     static public void main(String [] args) throws IOException, InterruptedException{
 
         //Build the server instance
         Server server = ServerBuilder.forPort(8080)
                 //add the service defined in the proto file
-                //.addService(true)
+                //.addService(MessageServiceU)
+                //.addService(MessageServiceB)
                 .build();
         server.start();
 
