b39a1c174ed48f8b7e7497da414815f5d70d47d8
Claudia Ortiz <claudia@graylog.com>
2019-03-12 16:54:24 -0500

    Add GRPCMessageServiceImplement

---
+++ src/main/java/com/example/grpc/GRPCMessageServiceImplement.java
@@ -0,0 +1,26 @@
+package com.example.grpc;
+
+
+import io.grpc.stub.StreamObserver;
+
+public class GRPCMessageServiceImplement extends MessageServiceUGrpc.MessageServiceUImplBase{
+
+    // Implement and build the MessageServiceU service found in .proto
+    public void messageRequestHandlerU(MessageRequest messageRequest, StreamObserver<MessageResponse> responseObserver) {
+
+        responseObserver.onNext(MessageResponse.newBuilder()
+                // Send message to client
+                .setMessage("Payload ")
+                .build());
+        responseObserver.onCompleted();
+
+        //Print request to client
+        System.out.println("Greeting Service Implemented " + messageRequest);
+    }
+
+    //Implement and build the MessageServiceB service found in .proto
+
+}
+
+
+
