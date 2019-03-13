df653671f0bcef8003ae00807f90fbf0e47190c6
Claudia Ortiz <claudia@graylog.com>
2019-03-13 14:52:52 -0500

    Create MessageRequest variable

--- src/main/java/com/example/grpc/GrpcClient.java
+++ src/main/java/com/example/grpc/GrpcClient.java
@@ -3,9 +3,12 @@ package com.example.grpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 public class GrpcClient {
 
-    public static void main(String [] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
+
 
         // Create a channel
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8443)
@@ -15,12 +18,16 @@ public class GrpcClient {
                 .usePlaintext(true)
                 .build();
 
-        // Unary Synchronous Blocking Stub
-        MessageServiceUGrpc.MessageServiceUBlockingStub uBlockingStub = MessageServiceUGrpc.newBlockingStub(channel);
-        uBlockingStub.messageServiceHandlerU(MessageRequest.newBuilder()
-                                                            .setMessage("Cerulean")
-                                                            .build());
+        MessageRequest messageRequest = MessageRequest.newBuilder()
+                .setMessage("This is an arbitrary message! :P ")
+                .build();
 
+        // Create Unary Synchronous Blocking Stub
+        MessageServiceUGrpc.MessageServiceUBlockingStub uBlockingStub = MessageServiceUGrpc.newBlockingStub(channel);
+        uBlockingStub.messageServiceHandlerU(messageRequest);
+
+
+        // Bidirectional Synchronous Blocking Stub
 //        MessageServiceBGrpc.MessageServiceBBlockingStub bBlockingStub = MessageServiceBGrpc.newBlockingStub(channel);
 //        bBlockingStub.messageServiceHandlerB(MessageRequest.newBuilder()
 //                                                            .setMessage("CeruleanCee")
