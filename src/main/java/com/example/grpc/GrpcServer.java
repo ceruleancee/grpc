81f97f72f815121f7107a8e7995c1b310ca886b6
Claudia Ortiz <claudia@graylog.com>
2019-03-13 14:54:53 -0500

    Update addService calls in GrpcServer

--- src/main/java/com/example/grpc/GrpcServer.java
+++ src/main/java/com/example/grpc/GrpcServer.java
@@ -1,4 +1,5 @@
 package com.example.grpc;
+import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
@@ -13,61 +14,19 @@ public class GrpcServer {
         //Build the server instance
         Server server = ServerBuilder.forPort(8443)
                 //add the service defined in the proto file
-                .addService(new GRPCMessageServiceImplU())  // Add unary service
-                .addService(new GRPCMessageServiceImplB())  // Add bidirectional service
+                .addService(new GRPCMessageServiceImpl.MessageServiceU())   // Add unary service
+                .addService(new GRPCMessageServiceImpl.MessageServiceB())  // Add bidirectional service
                 .build();
         server.start();
-        System.out.println("Hello World!");
+        System.out.println("Server is listening...");
+
+        // Something happens here
+
         server.awaitTermination();
-
-    }
-
-    // Unary Service Implemented
-    static class GRPCMessageServiceImplU extends MessageServiceUGrpc.MessageServiceUImplBase {
-
-        // Implement and build the MessageServiceU service found in .proto
-        public void messageRequestHandlerU(MessageRequest messageRequest, StreamObserver<MessageResponse> responseObserver) {
-            System.out.println("Hello World! " + messageRequest.getMessage());
-            String message = "Hello World! " + messageRequest.getMessage();
-
-            responseObserver.onNext(MessageResponse.newBuilder()
-                    // Send message to client
-                    .setMessage(messageRequest.toString())
-                    .build());
-            responseObserver.onCompleted();
-
-            //Print request to client
-            System.out.println("GRPCMessageServiceImplU works " + messageRequest);
-        }
     }
 
 
-    // Bidirectional Service Implemented
-    public static class GRPCMessageServiceImplB extends MessageServiceBGrpc.MessageServiceBImplBase {
 
-        // Implement and build the MessageServiceB service found in .proto
-         static LinkedHashSet<StreamObserver<MessageResponse>> observers = new LinkedHashSet<StreamObserver<MessageResponse>>();
 
-        public StreamObserver<MessageRequest>messageServiceHandlerB(StreamObserver<MessageResponse> responseObserver){
-            observers.add(responseObserver);
-            return  new StreamObserver<MessageRequest>() {
 
-                public void onNext(MessageRequest messageRequest) {
-                    MessageResponse messageResponse = MessageResponse.newBuilder()
-                            .setMessage(messageRequest.toString())
-                            .build();
-                    observers.forEach(o -> o.onNext(messageResponse));
-                }
-
-                public void onError(Throwable t) {
-                    observers.remove(responseObserver);
-                }
-
-                public void onCompleted() {
-                    observers.remove(responseObserver);
-                }
-            };
-        }
-
-    }
 }
