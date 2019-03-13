e3c58d437da02a217311557f318a65072f0347b1
Claudia Ortiz <claudia@graylog.com>
2019-03-13 11:23:01 -0500

    Add unary and bidirectional functionality to GrpcServer

--- src/main/java/com/example/grpc/GrpcServer.java
+++ src/main/java/com/example/grpc/GrpcServer.java
@@ -19,4 +19,53 @@ public class GrpcServer {
         server.start();
         server.awaitTermination();
     }
+
+    // Unary Service Implemented
+    static class GRPCMessageServiceImplU extends MessageServiceUGrpc.MessageServiceUImplBase {
+
+        // Implement and build the MessageServiceU service found in .proto
+        public void messageRequestHandlerU(MessageRequest messageRequest, StreamObserver<MessageResponse> responseObserver) {
+            System.out.println("Hello World! " + messageRequest.getMessage());
+            String message = "Hello World! " + messageRequest.getMessage();
+
+            responseObserver.onNext(MessageResponse.newBuilder()
+                    // Send message to client
+                    .setMessage(messageRequest.toString())
+                    .build());
+            responseObserver.onCompleted();
+
+            //Print request to client
+            System.out.println("GRPCMessageServiceImplU works " + messageRequest);
+        }
+    }
+
+
+    // Bidirectional Service Implemented
+    public static class GRPCMessageServiceImplB extends MessageServiceBGrpc.MessageServiceBImplBase {
+
+        // Implement and build the MessageServiceU service found in .proto
+         static LinkedHashSet<StreamObserver<MessageResponse>> observers = new LinkedHashSet<StreamObserver<MessageResponse>>();
+
+        public StreamObserver<MessageRequest>MessageServiceHandlerB(StreamObserver<MessageResponse> responseObserver){
+            observers.add(responseObserver);
+            return  new StreamObserver<MessageRequest>() {
+
+                public void onNext(MessageRequest messageRequest) {
+                    MessageResponse messageResponse = MessageResponse.newBuilder()
+                            .setMessage(messageRequest.toString())
+                            .build();
+                    observers.forEach(o -> o.onNext(messageResponse));
+                }
+
+                public void onError(Throwable t) {
+                    observers.remove(responseObserver);
+                }
+
+                public void onCompleted() {
+                    observers.remove(responseObserver);
+                }
+            };
+        }
+
+    }
 }
