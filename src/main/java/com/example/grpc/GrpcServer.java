c4ee58e92d5f7f38e53c9d6920568d6c178a6ad5
Claudia Ortiz <claudia@graylog.com>
2019-03-14 13:30:17 -0500

    Add basic SSL/TLS option to GrpcServer

--- src/main/java/com/example/grpc/GrpcServer.java
+++ src/main/java/com/example/grpc/GrpcServer.java
@@ -6,15 +6,20 @@ package com.example.grpc;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
+import java.io.File;
 import java.io.IOException;
 
 public class GrpcServer {
 
     static public void main(String[] args) throws IOException, InterruptedException {
 
+        File certChainFile = new File("/PATH/TO/certificate.pem");
+        File privateKeyFile = new File("/PATH/TO/key.pem");
+
         //Build the server instance
         Server server = ServerBuilder.forPort(8443)
                 //add the service defined in the proto file
+                .useTransportSecurity(certChainFile, privateKeyFile)
                 .addService(new GRPCMessageServiceImpl.MessageServiceU())   // Add unary service
                 .addService(new GRPCMessageServiceImpl.MessageServiceB())  // Add bidirectional service
                 .build();
