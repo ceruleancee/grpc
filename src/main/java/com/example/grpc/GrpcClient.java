4f2881694c8f8b5f5f5e07c901a91aee0c990483
Claudia Ortiz <claudia@graylog.com>
2019-03-14 13:29:30 -0500

    Add basic SSL/TLS option to GrpcClient

--- src/main/java/com/example/grpc/GrpcClient.java
+++ src/main/java/com/example/grpc/GrpcClient.java
@@ -4,24 +4,29 @@
 package com.example.grpc;
 
 import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import javax.net.ssl.SSLException;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class GrpcClient {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, SSLException {
+
+        File certChainFile = new File("/PATH/TO/certificate.pem");
 
         // Create a channel
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8443)
-
-                // .useTransportSecurity() // Security option
+        ManagedChannel channel = NettyChannelBuilder.forAddress("localhost", 8443)
+                .sslContext(GrpcSslContexts
+                        .forClient()
+                        .trustManager(certChainFile)
+                        .build())
                 // .nameResolverFactory() // For service registry
                 // .loadBalancerFactory() // Client side load balancer
-                .usePlaintext(true)
                 .build();
-
 
         MessageRequest messageRequest = MessageRequest.newBuilder()
                 .setMessage("This is an arbitrary message!")
@@ -31,17 +36,14 @@ public class GrpcClient {
         MessageServiceUGrpc.MessageServiceUBlockingStub uBlockingStub = MessageServiceUGrpc.newBlockingStub(channel);
         uBlockingStub.messageServiceHandlerU(messageRequest);
 
-
         // Bidirectional Synchronous Blocking Stub
         MessageServiceBGrpc.MessageServiceBStub bStub = MessageServiceBGrpc.newStub(channel);
         StreamObserver<MessageRequest> clientSideRequestStream = bStub.messageServiceHandlerB(buildResponseHandler());
         for (int i = 0; i < 10000; i++) {
-            System.out.println(i + " : " + messageRequest);
+            System.out.println(i + ": " + messageRequest);
             clientSideRequestStream.onNext(messageRequest);
-
         }
         channel.awaitTermination(10000, TimeUnit.DAYS);
-
     }
 
     private static StreamObserver<MessageResponse> buildResponseHandler() {
